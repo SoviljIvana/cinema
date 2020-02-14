177439e3e8195ed77357783fd5e0b37fb5387ba6
Administrator <Administrator@ZRKURS09>
2020-02-14 14:28:12 +0100

    update

---
+++ WinterWorkShop.Cinema.API/Controllers/CinemasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using WinterWorkShop.Cinema.API.Models;
+using WinterWorkShop.Cinema.Domain.Common;
+using WinterWorkShop.Cinema.Domain.Interfaces;
+using WinterWorkShop.Cinema.Domain.Models;
+
+namespace WinterWorkShop.Cinema.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CinemasController : ControllerBase
+    {
+        private readonly ICinemaService _cinemaService;
+
+        public CinemasController(ICinemaService cinemaService)
+        {
+            _cinemaService = cinemaService;
+        }
+
+        /// <summary>
+        /// Gets all cinemas
+        /// </summary>
+        /// <returns>List of cinemas</returns>
+        [HttpGet]
+        [Route("all")]
+        public async Task<ActionResult<IEnumerable<CinemaDomainModel>>> GetAsync()
+        {
+            IEnumerable<CinemaDomainModel> cinemaDomainModels;
+
+            cinemaDomainModels = await _cinemaService.GetAllAsync();
+
+            if (cinemaDomainModels == null)
+            {
+                cinemaDomainModels = new List<CinemaDomainModel>();
+            }
+
+            return Ok(cinemaDomainModels);
+        }
+    }
+}
---
+++ WinterWorkShop.Cinema.Domain/Services/CinemaService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WinterWorkShop.Cinema.Domain.Interfaces;
+using WinterWorkShop.Cinema.Domain.Models;
+using WinterWorkShop.Cinema.Repositories;
+
+namespace WinterWorkShop.Cinema.Domain.Services
+{
+    public class CinemaService : ICinemaService
+    {
+        private readonly ICinemasRepository _cinemasRepository;
+
+        public CinemaService(ICinemasRepository cinemasRepository)
+        {
+            _cinemasRepository = cinemasRepository;
+        }
+
+        public async Task<IEnumerable<CinemaDomainModel>> GetAllAsync()
+        {
+            var data = await _cinemasRepository.GetAll();
+
+            if (data == null)
+            {
+                return null;
+            }
+
+            List<CinemaDomainModel> result = new List<CinemaDomainModel>();
+            CinemaDomainModel model;
+            foreach (var item in data)
+            {
+                model = new CinemaDomainModel
+                {
+                    Id = item.Id,
+                    Name = item.Name
+                };
+                result.Add(model);
+            }
+
+            return result;
+        }
+    }
+
+}
