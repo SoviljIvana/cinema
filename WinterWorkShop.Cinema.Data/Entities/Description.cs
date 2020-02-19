47944103b4722bae9688e9171fb2fc560f7a15e3
Administrator <Administrator@ZRKURS09>
2020-02-19 09:45:57 +0100

    description entity added

---
+++ WinterWorkShop.Cinema.Data/Entities/Description.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WinterWorkShop.Cinema.Data.Entities
+{
+    [Table("description")]
+    public class Description
+    {
+        public Guid DescriptionId { get; set; }
+        public Guid MovieId { get; set; }
+        public int TagId { get; set; }
+
+    }
+}
