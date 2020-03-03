b19f9b50d8d986cadc90dad866c0392e3e644c63
JELENA ZUBAC <jzubacj@gmail.com>
2020-03-03 11:13:03 +0100

    GetAllTags

---
+++ WinterWorkShop.Cinema.Domain/Models/TagDomainModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinterWorkShop.Cinema.Domain.Models
+{
+    public class TagDomainModel
+    {
+        public List<string> genres { get; set; }
+        public List<string> actores { get; set; }
+        public List<string> creators { get; set; }
+        public List<string> languages { get; set; }
+        public List<string> states { get; set; }
+        public List<string> awords { get; set; }
+    }
+}
---
+++ WinterWorkShop.Cinema.Domain/Services/TagService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WinterWorkShop.Cinema.Domain.Interfaces;
+using WinterWorkShop.Cinema.Domain.Models;
+using WinterWorkShop.Cinema.Repositories;
+
+namespace WinterWorkShop.Cinema.Domain.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly ITagRepository _tagRepository;
+        public TagService(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        public async Task<TagDomainModel> GetAllTags()
+        {
+            var data = await _tagRepository.GetAll();
+            if (data == null)
+            {
+                return null;
+            }
+
+            TagDomainModel listOfTags = new TagDomainModel()
+            {
+                actores = new List<string>(),
+                awords = new List<string>(),
+                creators = new List<string>(),
+                genres = new List<string>(),
+                languages = new List<string>(),
+                states = new List<string>()
+            };
+
+            foreach (var item in data)
+            {
+                if (item.Type == "actor")
+                {
+                    listOfTags.actores.Add(item.Name);
+                }
+                if (item.Type == "aword")
+                {
+                    listOfTags.awords.Add(item.Name);
+                }
+                if (item.Type == "director")
+                {
+                    listOfTags.creators.Add(item.Name);
+                }
+                if (item.Type == "genre")
+                {
+                    listOfTags.genres.Add(item.Name);
+                }
+                if (item.Type == "language")
+                {
+                    listOfTags.languages.Add(item.Name);
+                }
+                if (item.Type == "state")
+                {
+                    listOfTags.states.Add(item.Name);
+                }
+            }
+
+            return listOfTags;
+        }
+
+        
+
+
+    }
+}
