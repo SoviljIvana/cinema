ee6e0e9899572a033da99558d35041246fc33aac
Administrator <Administrator@ZRKURS09>
2020-03-09 13:43:59 +0100

    added payment

--- WinterWorkShop.Cinema.ClientApp/cinema/src/App.js
+++ WinterWorkShop.Cinema.ClientApp/cinema/src/App.js
@@ -12,6 +12,7 @@ import UserProfile from './components/UserProfile';
 import ProjectionDetails from './components/user/ProjectionDetails';
 import AllProjectionsForCinema from './components/user/AllProjectionsForCinema';
 import AllForProjection from './components/user/AllForProjection';
+import Tickets from './components/user/Tickets';
 
 import Dashboard from './components/admin/Dashboard';
 
@@ -31,6 +32,8 @@ function App() {
         <Route path="/projectiondetails/allForProjection/:id" component={AllForProjection} />
         <PrivateRouteAdmin path="/dashboard" component={Dashboard} />
         <PrivateRouteAdmin path="/userProfile" component={UserProfile} />
+        <PrivateRouteAdmin path="/tickets" component={Tickets} />
+
       </Switch>
       <NotificationContainer />
       </div>
