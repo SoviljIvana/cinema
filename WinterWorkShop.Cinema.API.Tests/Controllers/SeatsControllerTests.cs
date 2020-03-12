3c95d698e72b10dfc1cd8ca12c7909ed65da3f57
JELENA ZUBAC <jzubacj@gmail.com>
2020-03-12 13:02:28 +0100

    SeatController tests

---
+++ WinterWorkShop.Cinema.API.Tests/Controllers/SeatsControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WinterWorkShop.Cinema.API.Controllers;
+using WinterWorkShop.Cinema.Domain.Interfaces;
+using WinterWorkShop.Cinema.Domain.Models;
+
+namespace WinterWorkShop.Cinema.Tests.Controllers
+{
+    [TestClass]
+    public class SeatsControllerTests
+    {
+        private Mock<ISeatService> _mockSeatService;
+        private List<RowsDomainModel> _rows;
+        private RowsDomainModel _rowsDomainModel;
+        private SeatDomainModel _seatDomainModel;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _seatDomainModel = new SeatDomainModel()
+            {
+                Id = Guid.NewGuid()
+            };
+            _rowsDomainModel = new RowsDomainModel()
+            {
+                SeatsInRow = new List<SeatDomainModel>()
+            };
+            _rowsDomainModel.SeatsInRow.Add(_seatDomainModel);
+            _rows = new List<RowsDomainModel>();
+            _rows.Add(_rowsDomainModel);
+            _mockSeatService = new Mock<ISeatService>();
+        }
+
+        [TestMethod]
+        public void SeatsController_GetAllSeatsForSpecificProjection_Returns_NotFoundMessage() 
+        {
+            //Arrange
+            IEnumerable<RowsDomainModel> rowsDomainModels = null;
+            Task<IEnumerable<RowsDomainModel>> responseTask = Task.FromResult(rowsDomainModels);
+            int expectedStatusCode = 404;
+
+            _mockSeatService = new Mock<ISeatService>();
+            _mockSeatService.Setup(x => x.GetAllSeatsForProjection(It.IsAny<Guid>())).Returns(responseTask);
+            SeatsController seatsController = new SeatsController(_mockSeatService.Object);
+            //Act
+            var resultAction = seatsController.GetAllSeatsForSpecificProjection(It.IsAny<Guid>()).ConfigureAwait(false).GetAwaiter().GetResult().Result;
+            var result = ((NotFoundObjectResult)resultAction).Value;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(resultAction, typeof(NotFoundObjectResult));
+            Assert.AreEqual(expectedStatusCode, ((NotFoundObjectResult)resultAction).StatusCode);
+        }
+        [TestMethod]
+        public void SeatsController_GetAllSeatsForSpecificProjection_Returns_OkObjectResult_RowsWithSeats()
+        {
+            //Arrange
+            IEnumerable<RowsDomainModel> rowsDomainModels = _rows;
+            Task<IEnumerable<RowsDomainModel>> responseTask = Task.FromResult(rowsDomainModels);
+            int expectedStatusCode = 200;
+            int expectedResultCount = 1;
+
+            _mockSeatService = new Mock<ISeatService>();
+            _mockSeatService.Setup(x => x.GetAllSeatsForProjection(It.IsAny<Guid>())).Returns(responseTask);
+            SeatsController seatsController = new SeatsController(_mockSeatService.Object);
+            //Act
+            var resultAction = seatsController.GetAllSeatsForSpecificProjection(It.IsAny<Guid>()).ConfigureAwait(false).GetAwaiter().GetResult().Result;
+            var result = ((OkObjectResult)resultAction).Value;
+            var rows = (List<RowsDomainModel>)result;
+            //Assert
+            Assert.IsNotNull(rows);
+            Assert.AreEqual(expectedResultCount, rows.Count);
+            Assert.IsInstanceOfType(resultAction, typeof(OkObjectResult));
+            Assert.AreEqual(expectedStatusCode, ((OkObjectResult)resultAction).StatusCode);
+            Assert.AreEqual(rows[0].SeatsInRow[0].Id, _rows[0].SeatsInRow[0].Id);
+        }
+    }
+}
--- WinterWorkShop.Cinema.API/Controllers/SeatsController.cs
+++ WinterWorkShop.Cinema.API/Controllers/SeatsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using WinterWorkShop.Cinema.Domain.Common;
 using WinterWorkShop.Cinema.Domain.Interfaces;
 using WinterWorkShop.Cinema.Domain.Models;
 
@@ -19,29 +20,7 @@ namespace WinterWorkShop.Cinema.API.Controllers
         {
             _seatService = seatService;
         }
-        //TODO: ovo se nigde ne koristi
-        /// <summary>
-        /// Gets all seats
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        [Route("all")]
-        public async Task<ActionResult<IEnumerable<SeatDomainModel>>> GetAsync()
-        {
-            IEnumerable<SeatDomainModel> seatDomainModels;
-            
-            seatDomainModels = await _seatService.GetAllAsync();
 
-            if (seatDomainModels == null)
-            {
-                seatDomainModels = new List<SeatDomainModel>();
-            }
-
-            return Ok(seatDomainModels);
-        }
-
-
-        //TODO: prebaciti u drugi kontroler!
         /// <summary>
         /// Returns all seats for a specific projection
         /// </summary>
@@ -51,17 +30,16 @@ namespace WinterWorkShop.Cinema.API.Controllers
         [Route("allForProjection/{projectionId}")]
         public async Task<ActionResult<IEnumerable<RowsDomainModel>>> GetAllSeatsForSpecificProjection(Guid projectionId)
         {
-            IEnumerable<RowsDomainModel> seatDomainModels;
+            IEnumerable<RowsDomainModel> rowsDomainModels;
 
+            rowsDomainModels = await _seatService.GetAllSeatsForProjection(projectionId);
 
-            seatDomainModels = await _seatService.GetAllSeatsForProjection(projectionId);
+            if (rowsDomainModels == null)
+            {
+                return NotFound(Messages.SEAT_GET_ALL_SEATS_ERROR);
+            }
 
-            //if (seatDomainModels == null)
-            //{
-            //    seatDomainModels = new List<SeatDomainModel>();
-            //}
-
-            return Ok(seatDomainModels);
+            return Ok(rowsDomainModels);
         }
     }
 
